ec9984321413c459b5dbfc82804b181ed69efe16
juhyun <zzang9ha@naver.com>
2023-03-19 10:12:04 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.9"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "com.heyd"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    compileOnly("org.projectlombok:lombok")
+	runtimeOnly("com.h2database:h2")
+
+    annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
