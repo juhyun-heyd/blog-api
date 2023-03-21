559ff4a6a9f2b056babcb43e1b629861f798aa37
juhyun <zzang9ha@naver.com>
2023-03-22 00:48:10 +0900

    feat: Add Constant class

---
+++ src/main/java/com/heyd/blogapi/common/Constant.java
@@ -0,0 +1,11 @@
+package com.heyd.blogapi.common;
+
+public class Constant {
+
+    private Constant() { }
+
+    public static final long POPULAR_KEYWORD_LIST_START = 0;
+
+    public static final long POPULAR_KEYWORD_LIST_END = 9;
+
+}
