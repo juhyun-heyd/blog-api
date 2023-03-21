0d0b317e94244a7c53424c1f8829311f9f78834f
juhyun <zzang9ha@naver.com>
2023-03-22 00:39:11 +0900

    feat: Add PopularKeyword Entity

---
+++ src/main/java/com/heyd/blogapi/domain/entity/PopularKeyword.java
@@ -0,0 +1,40 @@
+package com.heyd.blogapi.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PopularKeyword extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String keyword;
+
+    @Column
+    private Long count;
+
+    public PopularKeyword(String keyword) {
+        this(keyword, 0L);
+    }
+
+    public PopularKeyword(String keyword, Long count) {
+        this.keyword = keyword;
+        this.count = count;
+    }
+
+    public void incrementCount() {
+        this.count += 1;
+    }
+}
