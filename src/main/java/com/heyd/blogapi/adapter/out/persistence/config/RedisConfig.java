420da5c1cf18c7b15ea4850ada2198eae9b1a764
juhyun <zzang9ha@naver.com>
2023-03-21 23:52:36 +0900

    feat: Add RedisConfig

---
+++ src/main/java/com/heyd/blogapi/adapter/out/persistence/config/RedisConfig.java
@@ -0,0 +1,51 @@
+package com.heyd.blogapi.adapter.out.persistence.config;
+
+import com.heyd.blogapi.adapter.out.persistence.config.properties.RedisProperties;
+import lombok.RequiredArgsConstructor;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(
+                redisProperties.getHost(),
+                Integer.parseInt(redisProperties.getPort())
+        );
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        var redisTemplate = new RedisTemplate<String, String>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        var redisConfig = new Config();
+        redisConfig.useSingleServer()
+                .setAddress("redis://" + redisProperties.getHost() + ":" + redisProperties.getPort());
+
+        return Redisson.create(redisConfig);
+    }
+
+}
