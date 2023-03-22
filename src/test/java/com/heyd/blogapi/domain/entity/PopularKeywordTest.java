d371fee0c73e330c581d58009951b40891004aee
juhyun <zzang9ha@naver.com>
2023-03-22 17:55:04 +0900

    refactor: 필드 유효성 검증 로직 테스트 추가

--- src/test/java/com/heyd/blogapi/domain/entity/PopularKeywordTest.java
+++ src/test/java/com/heyd/blogapi/domain/entity/PopularKeywordTest.java
@@ -3,6 +3,7 @@ package com.heyd.blogapi.domain.entity;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PopularKeywo가rdTest {
 
@@ -13,6 +14,14 @@ class PopularKeywo가rdTest {
         entity.incrementCount();
 
         assertThat(entity.getCount()).isEqualTo(1L);
+    }
+
+    @Test
+    void 객체생성시_유효성검증에_실패하면_예외를_반환한다() {
+        assertThrows(
+                IllegalStateException.class,
+                () -> new PopularKeyword("keyword", -10L)
+        );
     }
 
 }
